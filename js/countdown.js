2ed3d02f3ab8000a72e4607b40c8c4c8d60d63b3
JunjoSick <78446819+JunjoSick@users.noreply.github.com>
2022-06-07 16:12:23 +0200

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // The data/time we want to countdown to
-var countDownDate = new Date(2022, 9, 24, 9).getTime();
+var countDownDate = new Date(2022, 9, 24, 9, 30, 0).getTime();
 var countElement = document.getElementById("count");
 // Run myfunc every second
 var myfunc = setInterval(() => {
